484f9244e9e46fd0660e691cdf5431a69092a6db
Brendan Younger <byounger@flur.ee>
2017-05-26 16:15:50 -0400

    Update homepage setting to serve from /template-react

--- index.js
+++ index.js
@@ -10,5 +10,7 @@ const app = express();
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 8000;
 
 app.use(express.static('build'));
+app.use('/template-react', express.static('build'));
+
 app.post('/api/fluree.template-react.echo', bodyParser.json(), echo);
 app.listen(port, () => console.log('Listening on port', port));
